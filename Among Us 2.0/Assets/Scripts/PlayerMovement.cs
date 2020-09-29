d56c10e76e1ea2acb40b45193df380bf3fe790f9
ivy997 <49655087+ivy997@users.noreply.github.com>
2020-09-29 17:14:31 +0300

    Initial Commit
    -Add script for character movement
    -Add script for camera movement
    -Add script for interaction with objects
    -Add background
    -Add plain player sprite

---
+++ Among Us 2.0/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rigidBody;
+    private Vector3 position;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCharacter();
+        //UpdateAnimation();
+    }
+
+    void UpdateAnimation()
+    {
+        if (position != Vector3.zero)
+        {
+            animator.SetFloat("moveX", position.x);
+            animator.SetFloat("moveY", position.y);
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        position = Vector3.zero;
+        position.x = Input.GetAxisRaw("Horizontal");
+        position.y = Input.GetAxisRaw("Vertical");
+
+        if (position.magnitude > 1)
+        {
+            position = Vector3.ClampMagnitude(position, 1);
+        }
+
+        rigidBody.MovePosition(transform.position + position * speed * Time.deltaTime);
+    }
+}
